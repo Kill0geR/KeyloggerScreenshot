9116f1161fbd47926c9e85e333bb56ab41a3732f
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-04-26 00:41:23 +0200

    Update Simulation_code.py

--- KeyloggerScreenshot/Simulation_code.py
+++ KeyloggerScreenshot/Simulation_code.py
@@ -61,11 +61,7 @@ class Simulation:
             data = subprocess.check_output("tasklist")
             str_data = str(data).split()
             all_exe = [(exe.replace(r"K\\r\\n", "").replace(r"K\r\n", ""), str_data[idx + 1]) for idx, exe in enumerate(str_data) if ".exe" in exe]
-            photo = []
-            for task, pid in all_exe:
-                if task == "PhotosApp.exe":
-                    photo.append((task, pid))
-
+            photo = [(task, pid) for task, pid in all_exe if task == "PhotosApp.exe"]
             if photo:
                 os.system(f"taskkill /f /PID {photo[0][1]}")
         os._exit(0)
